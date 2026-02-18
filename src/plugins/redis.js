e2a174eb81ab067655dd05d852b0b63ac829d4b3
Ziad Sameh <ziad.s.a.m.e.h223@gmail.com>
2026-02-18 14:59:43 +0200

    better error handling reddis

--- src/plugins/redis.js
+++ src/plugins/redis.js
@@ -4,12 +4,22 @@ import Redis from 'ioredis';
 async function redisPlugin(fastify) {
   const redis = new Redis(process.env.REDIS_URL, {
     maxRetriesPerRequest: 3,
-    retryDelayOnFailover: 100,
-    tls: process.env.REDIS_URL?.startsWith('rediss://') ? {} : undefined,
+    retryStrategy(times) {
+      const delay = Math.min(times * 500, 5000);
+      return delay;
+    },
+    lazyConnect: true,
   });
 
   redis.on('connect', () => fastify.log.info('Redis connected'));
-  redis.on('error', (err) => fastify.log.error('Redis error:', err));
+  redis.on('error', (err) => fastify.log.error('Redis error: %s', err.message));
+
+  // Try to connect but don't block startup
+  try {
+    await redis.connect();
+  } catch (err) {
+    fastify.log.warn('Redis initial connection failed, will retry: %s', err.message);
+  }
 
   fastify.decorate('redis', redis);
   fastify.addHook('onClose', () => redis.quit());
